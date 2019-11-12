13c0ba0a00987779d054e639bf9effcf85e0200b
seansiry <sean.siry@gmail.com>
2019-11-12 18:38:25 -0500

    Commit Number 3

--- app/src/main/java/com/to626/hw4/ReportActivity.java
+++ app/src/main/java/com/to626/hw4/ReportActivity.java
@@ -1,11 +1,17 @@
 package com.to626.hw4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -39,5 +45,22 @@ public class ReportActivity extends AppCompatActivity implements View.OnClickLis
             BirdSighting createSighting = new BirdSighting(bird, zipcode, name);
             myRef.push().setValue(createSighting);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.item_report){
+            Toast.makeText(this, "Already in Report Sighting Screen", Toast.LENGTH_SHORT).show();
+        } else if(item.getItemId() == R.id.item_search){
+            startActivity(new Intent(ReportActivity.this, SearchActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/to626/hw4/SearchActivity.java
+++ app/src/main/java/com/to626/hw4/SearchActivity.java
@@ -1,12 +1,18 @@
 package com.to626.hw4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -37,5 +43,22 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
 
 
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.item_search){
+            Toast.makeText(this, "Already in Search Screen", Toast.LENGTH_SHORT).show();
+        } else if(item.getItemId() == R.id.item_report){
+            startActivity(new Intent(SearchActivity.this, ReportActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
