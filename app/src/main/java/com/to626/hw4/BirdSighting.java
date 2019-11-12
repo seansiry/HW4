eff8c7960db977c26bb37d1f9e4739637ee60b19
seansiry <sean.siry@gmail.com>
2019-11-12 14:44:23 -0500

    Commit Number 3

---
+++ app/src/main/java/com/to626/hw4/BirdSighting.java
@@ -0,0 +1,17 @@
+package com.to626.hw4;
+
+public class BirdSighting {
+    public String species;
+    public String zipcode;
+    public String sighter;
+
+    public BirdSighting() {
+
+    }
+
+    public BirdSighting(String species, String zipcode, String sighter) {
+        this.species = species;
+        this.zipcode = zipcode;
+        this.sighter = sighter;
+    }
+}
--- app/src/main/java/com/to626/hw4/ReportActivity.java
+++ app/src/main/java/com/to626/hw4/ReportActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ReportActivity extends AppCompatActivity  implements View.OnClickListener{
     Button submitButton;
     EditText birdEditText, nameEditText, zipcodeEditText;
@@ -21,14 +24,19 @@ public class ReportActivity extends AppCompatActivity  implements View.OnClickLi
         nameEditText = findViewById(R.id.editText_name);
         submitButton = findViewById(R.id.button_submit);
 
+        submitButton.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("birdSightings");
+
         String bird = birdEditText.getText().toString();
         String zipcode = zipcodeEditText.getText().toString();
         String name = nameEditText.getText().toString();
-
-
+        BirdSighting createSighting = new BirdSighting(bird, zipcode, name);
+        myRef.push().setValue(createSighting);
     }
 }
