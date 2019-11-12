4d389e7c92f652ad92cd9a8ffc5ab2b9612595a2
seansiry <sean.siry@gmail.com>
2019-11-12 09:33:27 -0500

    Commit Number 2

---
+++ app/src/main/java/com/to626/hw4/ReportActivity.java
@@ -0,0 +1,21 @@
+package com.to626.hw4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ReportActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
